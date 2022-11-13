fa0f248362adc14b3dba4d13b338e9b4707d2fdd
omarmhmmd <omar.mhmmd@gmail.com>
2022-11-13 12:21:55 -0800

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Bamyan</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="Bamyan" />
+        <meta property="og:description" content="A typeface" />
+        <meta property="og:image" content="images/meta.png" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
