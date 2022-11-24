2a5bfe459986a60d1ea13a743cd0da976ee3830a
omarmhmmd <omar.mhmmd@gmail.com>
2022-11-23 18:30:45 -0800

    added film section v1

--- pages/film/index.tsx
+++ pages/film/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "./film.module.css";
 import { motion } from "framer-motion";
+import Vimeo from "@u-wave/react-vimeo";
 
 export default function Film() {
   return (
@@ -14,7 +15,24 @@ export default function Film() {
         transition={{ duration: 1 }}
         className={styles.container}
       >
-        <img src="https://picsum.photos/200/300" alt="" />
+        <div className={styles.flex}>
+          <div className={styles.video}>
+            <Vimeo
+              className={styles.vimeo}
+              showTitle={false}
+              showPortrait={false}
+              showByline={false}
+              video="246914053"
+              autoplay
+            />
+          </div>
+          <div className={styles.text}>
+            <p>
+              Bamyan is a typeface exploring the cultural history of the great
+              Bamyan Valley of Afghanistan. - Omar
+            </p>
+          </div>
+        </div>
       </motion.div>
     </>
   );
