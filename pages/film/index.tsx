f76c6e4f639691d3a4561b922819552b13e15264
omarmhmmd <omar.mhmmd@gmail.com>
2022-12-07 18:40:45 -0800

    added stories 4

--- pages/film/index.tsx
+++ pages/film/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "./film.module.css";
 import { motion } from "framer-motion";
 import Vimeo from "@u-wave/react-vimeo";
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Film() {
   return (
@@ -24,7 +24,6 @@ export default function Film() {
               showPortrait={false}
               showByline={false}
               video="246914053"
-              autoplay
             />
             {/* <img src="images/meta.png" alt="" /> */}
           </div>
@@ -38,49 +37,55 @@ export default function Film() {
             </div>
             <div className={styles.text}>
               <p>
-                Salaam, and thank you for watching this film. My name is Omar
-                Mohammad. I am a designer based in Kabul, Afghanistan. Since
-                graduating from Kabul University this summer, I have been
-                working on contributing to the cultural design language of
-                Afghanistan. While studying Graphic Design at Kabul University,
-                I realized our country lacked a contemporary cultural design
-                language in many fields, especially architecture and graphic
-                design.
+                Salaam, and thank you for watching this film. I&apos;m Omar Mohammad,
+                a designer based in Kabul, Afghanistan. While studying Design at
+                Kabul University, the reality that our country lacks a
+                contemporary cultural design language in many fields, especially
+                architecture and graphic design, was a constant constraint in my
+                work and research. My main focus as a student and now as a
+                professional is about contributing to the cultural design
+                language of Afghanistan through personal and commercial work.
                 <br />
                 <br />
-                My thesis at university related to concepts about what Afghan
-                design is, what it looks like, and who its contributors are. The
-                Ministry of Information & Culture was invited to the graduate
-                exhibition show and took notice of my work and research. The
-                Ministry is currently searching for fresh thinkers and designers
-                to contribute to government funded design projects, at this
-                point mostly in the realm of tourism. With the sudden boom of
-                tourism to the country from the west, many of these projects are
-                geared towards boosting the cultural tourism economy in
-                Afghanistan.
-                <br /> <br />
+                My MFA thesis at university explored concepts surrounding the
+                definition of Afghan design, the artifacts of Afghan design, and
+                the history of Afghan design. Luckily the Ministry of
+                Information & Culture was invited to the graduate exhibition
+                show and took great notice of my work and research. The Ministry
+                is currently searching for fresh thinkers and designers to
+                contribute to government funded design projects. With the sudden
+                western boom of tourism coming to the country, many of these
+                projects are geared towards boosting the cultural tourism
+                economy of Afghanistan.
+                <br />
+                <br />
                 The Buddhas of Bamyan have been a mainstay for those in the
                 academic fields of archaeology, religion, and anthropology, yet
-                they have not been utilized as a means for drawing foreigners
-                and outsiders into the country. Some of my thesis work explored
-                graphic and visual identities in regards to this region,
-                activating forms in the valley and landscape into cultural
-                pieces of graphic design. The Minister of Information and
-                Culture himself, Ibrahim Nevin, took notice of this work and
-                commissioned me to design a project about Bamyan that
-                incorporated the work from my thesis. A cartoonist and
-                filmmaker, Minister Nevin requested the final artifact to be a
-                short film highlighting the beauty and
-                wonders of the Bamyan Valley and its Buddhas.
-                <br /> <br />
+                they have not been fully utilized as a means for drawing
+                foreigners and tourists into the country. Some of my thesis work
+                explored graphic and visual identities in regards to this
+                beautiful region, activating forms in the valley and landscape
+                into cultural pieces of graphic/visual output. The Minister of
+                Information and Culture, Ibrahim Nevin, took notice of this work
+                and commissioned me to design a project about Bamyan that
+                incorporated the work from my thesis. A cartoonist and filmmaker
+                himself, Minister Nevin requested the final artifact to be a
+                short film highlighting the beauty and wonders of the Bamyan
+                Valley and its Buddhas.
+                <br />
+                <br />
                 The film includes original music, custom graphics and
-                typography, and a personalized voiceover. It&apos;s not much, but
-                it&apos;s a first step towards creating a cultural design language
-                for Afghanistan.
-                <br /><br />
+                typography, and a personalized voiceover. I&apos;d like to thank my
+                classmates, colleagues, and professors at Kabul University for
+                their support throughout this project. It&apos;s not much, but it&apos;s a
+                first step towards creating a cultural design language for
+                Afghanistan.
+                <br />
+                <br />
                 – <Link target="_blank" href="https://omarmhmmd.com/">
-                  <span style={{fontWeight:"bold"}}>omarmhmmd</span>&nbsp;&nbsp;
-                  <span style={{fontFamily:"Farsi"}}>عمر محمد </span> 
+                  <span style={{ fontWeight: "bold" }}>omarmhmmd</span>
+                  &nbsp;&nbsp;
+                  <span style={{ fontFamily: "Farsi" }}>عمر محمد </span>
                 </Link>
               </p>
             </div>
